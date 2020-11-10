57f69aaf90fda7b421c73a53355ea89db672881d
Hai Nguyen <haidnguyen@kms-technology.com>
2020-11-10 10:48:08 +0700

    update

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
-import { animationFrameScheduler, defer, fromEvent, of, Subject } from 'rxjs';
-import { map, switchMapTo, takeUntil, throttleTime, withLatestFrom } from 'rxjs/operators';
+import { animationFrameScheduler, defer, fromEvent, interval, merge, of, Subject } from 'rxjs';
+import { map, scan, switchMap, switchMapTo, take, takeUntil, throttleTime, withLatestFrom } from 'rxjs/operators';
 
 @Component({
   selector: 'app-slider',
@@ -16,15 +16,29 @@ export class SliderComponent {
   mouseDown$ = new Subject<MouseEvent>();
   buttonStyle$ = this.mouseDown$.pipe(
     switchMapTo(
-      fromEvent<MouseEvent>(document, 'mousemove').pipe(
-        takeUntil(fromEvent(document, 'mouseup')),
-        throttleTime(0, animationFrameScheduler),
-        withLatestFrom(defer(() => of(this.slider.nativeElement.clientWidth))),
-        map(([moveEvent, sliderWidth]) => {
-          const position = moveEvent.clientX + 1 - 44;
-          return { 'left.px': position <= 1 ? 1 : Math.min(sliderWidth - 1 - 44, position) };
-        })
-      )
-    )
+      merge(
+        fromEvent<MouseEvent>(document, 'mousemove').pipe(
+          takeUntil(fromEvent(document, 'mouseup')),
+          throttleTime(0, animationFrameScheduler),
+          withLatestFrom(defer(() => of(this.slider.nativeElement.clientWidth))),
+          map(([moveEvent, sliderWidth]) => {
+            const position = moveEvent.clientX + 1 - 44;
+            return { 'left.px': position <= 1 ? 1 : Math.min(sliderWidth - 1 - 44, position) };
+          }),
+        ),
+        fromEvent<MouseEvent>(document, 'mouseup').pipe(
+          map(mouseupEvent => mouseupEvent.clientX + 1 - 44),
+          withLatestFrom(defer(() => of(this.slider.nativeElement.clientWidth))),
+          switchMap(([position, sliderWidth]) => {
+            const step = sliderWidth / 60;
+            return interval(0, animationFrameScheduler).pipe(
+              take(position / step + 1),
+              scan(acc => acc - step, Math.min(sliderWidth - 1 - 44, position)),
+              map(position => ({ 'left.px': position <= 1 ? 1 : position })),
+            );
+          }),
+        ),
+      ),
+    ),
   );
 }
